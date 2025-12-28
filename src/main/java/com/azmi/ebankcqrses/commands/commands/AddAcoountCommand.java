0200e8792a0459a91ca1c49ffda12a50912b664b
AzmiZakaria <zakariaazmi45@gmail.com>
2025-12-28 13:59:08 +0100

    add: annotation for Add account commande

--- src/main/java/com/azmi/ebankcqrses/commands/commands/AddAcoountCommand.java
+++ src/main/java/com/azmi/ebankcqrses/commands/commands/AddAcoountCommand.java
@@ -1,6 +1,12 @@
 package com.azmi.ebankcqrses.commands.commands;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Getter @AllArgsConstructor
 public class AddAcoountCommand {
+    @TargetAggregateIdentifier
     private String id;
     private double initialBallance;
     private double cuurency;
