355396580cc6801327278e7224dddf2daecc8f8c
AzmiZakaria <zakariaazmi45@gmail.com>
2025-12-28 20:45:55 +0100

    update Controller: add endpoit /credit

--- src/main/java/com/azmi/ebankcqrses/commands/controllers/AccountCommandController.java
+++ src/main/java/com/azmi/ebankcqrses/commands/controllers/AccountCommandController.java
@@ -1,20 +1,26 @@
 package com.azmi.ebankcqrses.commands.controllers;
 
 import com.azmi.ebankcqrses.commands.commands.AddAcoountCommand;
+import com.azmi.ebankcqrses.commands.commands.CreditAcoountCommand;
 import com.azmi.ebankcqrses.commands.dtos.AddNewAccountRequestDTO;
+import com.azmi.ebankcqrses.commands.dtos.CreditAccountRequestDTO;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/commands")
 public class AccountCommandController {
+    private final EventStore eventStore;
     private CommandGateway commandGateway;
 
-    public AccountCommandController(CommandGateway commandGateway) {
+    public AccountCommandController(CommandGateway commandGateway, EventStore eventStore) {
         this.commandGateway = commandGateway;
+        this.eventStore = eventStore;
     }
 
     @PostMapping("/add")
@@ -26,8 +32,21 @@ public class AccountCommandController {
         ));
         return response;
     }
+    @PostMapping("/credit")
+    public  CompletableFuture<String> creditAccountCommand(@RequestBody CreditAccountRequestDTO request){
+        CompletableFuture<String> response = commandGateway.send(new CreditAcoountCommand(
+                request.accountID(),
+                request.amount(),
+                request.curency()
+        ));
+        return response;
+    }
     @ExceptionHandler(Exception.class)
     public String execptionHandler(Exception exception){
         return exception.getMessage();
+    }
+    @GetMapping("/events/{accountId}")
+    public Stream eventStore(@PathVariable String accountId){
+        return eventStore.readEvents(accountId).asStream();
     }
 }
